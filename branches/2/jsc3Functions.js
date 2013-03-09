3359a16725925861e7b8c239a6aa1ae6f4135552
jszyman@gmail.com <jszyman@gmail.com@a1d9c3dc-f936-7c11-093f-bd7b5080730a>
2013-03-09 21:00:10 +0000

    burn down similar to SWF XML charts

--- branches/2/jsc3Functions.js
+++ branches/2/jsc3Functions.js
@@ -1,7 +1,9 @@
-var myData = new Array( ['11-Mar', 100], ['12-Mar', 75], ['13-Mar', 50], ['14-Mar', 25], ['15-Mar', 0]);
+var PerfLine = new Array( ['11-Mar', 100], ['12-Mar', 75], ['13-Mar', 50], ['14-Mar', 25], ['15-Mar', 0]);
+var WorkRemain = new Array( ['11-Mar', 100], ['12-Mar', 90], ['13-Mar', 65], ['14-Mar', 20], ['15-Mar', 4]);
 var myChart = new JSChart('chart_div', 'line');
 
-	myChart.setDataArray(myData);
+	myChart.setDataArray(PerfLine, 'perf_line');
+	myChart.setDataArray(WorkRemain, 'work_remain');
 	myChart.setTitle('Employee performance');
 	myChart.setTitleColor('#8E8E8E');
 	myChart.setTitleFontSize(11);
@@ -14,19 +16,20 @@ var myChart = new JSChart('chart_div', 'line');
 	myChart.setAxisPaddingTop(50);
 	myChart.setAxisPaddingBottom(40);
 	myChart.setAxisValuesDecimals(0);
-	myChart.setAxisValuesNumberX(myData.length);
+	myChart.setAxisValuesNumberX(PerfLine.length);
 	myChart.setShowXValues(false);
 	myChart.setGridColor('#C5A2DE');
-	myChart.setLineColor('#BBBBBB');
-	myChart.setLineWidth(2);
+	myChart.setLineColor('#00FF00', 'perf_line');
+	myChart.setLineColor('#FF0000', 'work_remain');
+    myChart.setLineWidth(2);
 	myChart.setFlagColor('#9D12FD');
 	myChart.setFlagRadius(4);
 
-myChart.setTooltip(['11-Mar', '100d']);
-myChart.setTooltip(['12-Mar', '75d']);
-myChart.setTooltip(['13-Mar', '50d']);
-myChart.setTooltip(['14-Mar', '25d']);
-myChart.setTooltip(['15-Mar', '0d']);
+myChart.setTooltip(['11-Mar', '100d', 'perf_line']);
+myChart.setTooltip(['12-Mar', '75d', 'perf_line']);
+myChart.setTooltip(['13-Mar', '50d', 'perf_line']);
+myChart.setTooltip(['14-Mar', '25d', 'perf_line']);
+myChart.setTooltip(['15-Mar', '0d', 'perf_line']);
 /*
 myChart.setLabelX(['11-Mar', '100']);
 myChart.setLabelX(['12-Mar', '100']);
