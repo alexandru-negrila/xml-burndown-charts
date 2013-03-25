b2a126b7f0b3ea7486f2edb824815606d8de323c
jszyman@gmail.com <jszyman@gmail.com@a1d9c3dc-f936-7c11-093f-bd7b5080730a>
2013-03-25 19:27:08 +0000

    extracting date from xml-file

--- branches/3/digest_req.rb
+++ branches/3/digest_req.rb
@@ -2,8 +2,11 @@
 # shall be executed on regular time basis (every day, week, etc)
 # XML report is stored in file and used for creation/update of xml-data file
 # for burn down chart
+
+#search current dir first for includes
+$:.unshift File.dirname(__FILE__)
 require 'rubygems'
-require 'nokogiri'   
+require 'nokogiri'
 
 DEBUG = 1
 
@@ -27,6 +30,44 @@ commts = []
 in_file.xpath("//comment()").each {|comment| commts << comment}
 
 print "Found #{commts.length} comment node(s).\n"
-puts commts[-1]
+print "Comment class: #{commts[0].class} \n"
+
+
+# check if file name matches with date reported by JIRA
+jira_rep_wday_regex = Regexp.new('at (Mon|Tue|Wed|Thu|Fri|Sat|Sun)')
+jira_rep_wday_regex.match(commts[0].to_s)
+jira_rep_wday = $1
+if jira_rep_wday != nil
+    puts jira_rep_wday
+else
+    puts "wday not found!"
+end
+
+jira_rep_month_regex = Regexp.new('(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)')
+jira_rep_month_regex.match(commts[0].to_s)
+jira_rep_month = $1
+if jira_rep_month != nil
+    puts jira_rep_month
+else
+    puts "wday not found!"
+end
+
+jira_rep_mday_regex = Regexp.new('at (\w{3}) (\w{3}) ([0-9]{2})')
+jira_rep_mday_regex.match(commts[0].to_s)
+jira_rep_mday = $3
+if jira_rep_mday != nil
+    puts jira_rep_mday
+else
+    puts "mday not found!"
+end
+
+jira_rep_year_regex = Regexp.new('([0-9]{4})$')
+jira_rep_year_regex.match(commts[0].to_s)
+jira_rep_year = $1
+if jira_rep_year != nil
+    puts jira_rep_year
+else
+    puts "year not found!"
+end
 
 __END__
