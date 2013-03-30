617ac560fa486a88b6d1e8f165e56ece9476b355
jszyman@gmail.com <jszyman@gmail.com@a1d9c3dc-f936-7c11-093f-bd7b5080730a>
2013-03-30 13:29:18 +0000

    validation of date from report added

--- branches/3/digest_req.rb
+++ branches/3/digest_req.rb
@@ -7,6 +7,7 @@
 $:.unshift File.dirname(__FILE__)
 require 'rubygems'
 require 'nokogiri'
+require 'jira_support'
 
 DEBUG = 1
 
@@ -22,52 +23,21 @@ else
 end
 
 dir = File.dirname(__FILE__)
-#in_file = File.new(dir+jira_resp_dir+jira_resp_file, "r")
 in_file = Nokogiri::XML(open(dir+jira_resp_dir+jira_resp_file))
 
 puts in_file.css("title").text
 commts = []
-in_file.xpath("//comment()").each {|comment| commts << comment}
+in_file.xpath("//comment()").each {|comment| commts << comment.to_s}
 
 print "Found #{commts.length} comment node(s).\n"
 print "Comment class: #{commts[0].class} \n"
 
 
 # check if file name matches with date reported by JIRA
-jira_rep_wday_regex = Regexp.new('at (Mon|Tue|Wed|Thu|Fri|Sat|Sun)')
-jira_rep_wday_regex.match(commts[0].to_s)
-jira_rep_wday = $1
-if jira_rep_wday != nil
-    puts jira_rep_wday
+if (0 == jira_sup_check_date(commts[0]))
+    puts "Date is correct!"
 else
-    puts "wday not found!"
-end
-
-jira_rep_month_regex = Regexp.new('(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)')
-jira_rep_month_regex.match(commts[0].to_s)
-jira_rep_month = $1
-if jira_rep_month != nil
-    puts jira_rep_month
-else
-    puts "wday not found!"
-end
-
-jira_rep_mday_regex = Regexp.new('at (\w{3}) (\w{3}) ([0-9]{2})')
-jira_rep_mday_regex.match(commts[0].to_s)
-jira_rep_mday = $3
-if jira_rep_mday != nil
-    puts jira_rep_mday
-else
-    puts "mday not found!"
-end
-
-jira_rep_year_regex = Regexp.new('([0-9]{4})$')
-jira_rep_year_regex.match(commts[0].to_s)
-jira_rep_year = $1
-if jira_rep_year != nil
-    puts jira_rep_year
-else
-    puts "year not found!"
+    puts "Incorrect date!"
 end
 
 __END__
